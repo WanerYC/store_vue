453f083b94952d33674529b57d53385b9dddbdfa
ChunYnag <yangchun920726@126.com>
2018-07-09 15:41:21 +0800

    头部完成布局
    头部完成布局

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Router from 'vue-router';
 // 导入组件
 import Login from '@/views/Login';
+import Home from '@/views/Home';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {name: 'login', path: '/login', component: Login},
-    {name: 'home', path: '/', redirect: {name: 'login'}}
+    {name: 'home', path: '/', component: Home}
+    // {}
   ]
 });
---
+++ src/views/Home.vue
@@ -0,0 +1,61 @@
+<template>
+    <el-container class="container">
+        <el-header class="header">
+            <el-row>
+                <el-col :span="4">
+                    <img src="/static/logo.png" alt="">
+                </el-col>
+                <el-col :span="19">
+                    <h2 class="title">电商管理后台</h2>
+                </el-col>
+                <el-col :span="1">
+                    <a href="#" class="loginout">退出</a>
+                </el-col>
+            </el-row>
+        </el-header>
+        <el-container class="aside">
+            <el-aside width="200px">Aside</el-aside>
+            <el-main class="main">Main</el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+.container {
+  height: 100%;
+}
+
+.header {
+  background-color: #b3c0d1;
+  padding: 0;
+}
+
+.title {
+    color: #fff;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    line-height: 60px;
+}
+
+.loginout {
+    line-height: 60px;
+    color: orange;
+    text-decoration: none;
+}
+
+.aside {
+  background-color: #d3dce6;
+}
+
+.main {
+  background-color: #e9eef3;
+  /* height: 100%; */
+}
+</style>
