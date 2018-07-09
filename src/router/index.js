439c927881a1882cc2ea459462ba8689d62b30f5
ChunYnag <yangchun920726@126.com>
2018-07-09 16:58:40 +0800

    内容区域卡片化和面包屑

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,21 @@ import Router from 'vue-router';
 // 导入组件
 import Login from '@/views/Login';
 import Home from '@/views/Home';
+import Users from '@/views/Users/Users.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {name: 'login', path: '/login', component: Login},
-    {name: 'home', path: '/', component: Home}
-    // {}
+    {
+      name: 'home',
+      path: '/',
+      component: Home,
+      children: [
+        // 用户列表
+        {name: 'users', path: '/users', component: Users}
+      ]
+    }
   ]
 });
