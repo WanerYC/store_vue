dfae407668accf59a4a4b2123c0889018ee3dfd7
ChunYnag <yangchun920726@126.com>
2018-07-11 20:29:57 +0800

    权限列表--页面搭建

--- src/views/roles/Rights.vue
+++ src/views/roles/Rights.vue
@@ -1,15 +1,56 @@
 <template>
-  <div>
-    hi here is rights
-  </div>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right" class="elbreadcrumb">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--  内容表格 -->
+    <el-table
+      border
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="权限名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="路径"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="层级">
+      </el-table-column>
+    </el-table>
+
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: []
+    }
+  }
 };
 </script>
 
 <style>
+.box-card {
+  height: 100%;
+}
 
+.elbreadcrumb {
+  margin-bottom: 10px;
+}
 </style>
