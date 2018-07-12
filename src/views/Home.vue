714cf8de6de2404f107ea81a7ce2577703d82a9e
ChunYnag <yangchun920726@126.com>
2018-07-12 20:30:33 +0800

    动态菜单
    动态菜单

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,66 +21,22 @@
                 :router="true"
                 default-active="1-1"
                 class="menu">
-                    <el-submenu index="1">
+                    <!-- 一级菜单 -->
+                    <el-submenu
+                        :index="item.id + ''"
+                        v-for="item in menus"
+                        :key="item.id">
                         <template slot="title">
                         <i class="el-icon-location"></i>
-                        <span>用户管理</span>
+                        <span>{{item.authName}}</span>
                         </template>
-                        <el-menu-item index="/users">
+                        <!-- 二级菜单 -->
+                        <el-menu-item
+                            :index="'/'+item1.path"
+                            v-for="item1 in item.children"
+                            :key="item1.id">
                             <i class="el-icon-menu"></i>
-                            用户列表
-                        </el-menu-item>
-                    </el-submenu>
-                    <el-submenu index="2">
-                        <template slot="title">
-                        <i class="el-icon-location"></i>
-                        <span>权限管理</span>
-                        </template>
-                        <el-menu-item index="/roles">
-                            <i class="el-icon-menu"></i>
-                            角色列表
-                        </el-menu-item>
-                        <el-menu-item index="/rights">
-                            <i class="el-icon-menu"></i>
-                            权限列表
-                        </el-menu-item>
-                    </el-submenu>
-                    <el-submenu index="3">
-                        <template slot="title">
-                        <i class="el-icon-location"></i>
-                        <span>商品管理</span>
-                        </template>
-                        <el-menu-item index="3-1">
-                            <i class="el-icon-menu"></i>
-                            商品列表
-                        </el-menu-item>
-                        <el-menu-item index="3-2">
-                            <i class="el-icon-menu"></i>
-                            分类参数
-                        </el-menu-item>
-                        <el-menu-item index="3-3">
-                            <i class="el-icon-menu"></i>
-                            商品分类
-                        </el-menu-item>
-                    </el-submenu>
-                    <el-submenu index="4">
-                        <template slot="title">
-                        <i class="el-icon-location"></i>
-                        <span>订单管理</span>
-                        </template>
-                        <el-menu-item index="4-1">
-                            <i class="el-icon-menu"></i>
-                            订单列表
-                        </el-menu-item>
-                    </el-submenu>
-                    <el-submenu index="5">
-                        <template slot="title">
-                        <i class="el-icon-location"></i>
-                        <span>数据统计</span>
-                        </template>
-                        <el-menu-item index="5-1">
-                            <i class="el-icon-menu"></i>
-                            数据报表
+                            {{ item1.authName }}
                         </el-menu-item>
                     </el-submenu>
                 </el-menu>
@@ -95,6 +51,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      menus: []
+    };
+  },
   // 判断是否登录
   beforeCreate() {
     // 从sessionStorage 获取token判断是否有
@@ -105,7 +66,20 @@ export default {
       this.$message.warning('请登录');
     }
   },
+  created() {
+    this.loadData();
+  },
   methods: {
+    async loadData() {
+      const { data: resData } = await this.$http.get('menus');
+      const { data, meta: { status, msg } } = resData;
+      console.log(data);
+      if (status === 200) {
+        this.menus = data;
+      } else {
+        this.$message.error(msg);
+      }
+    },
     // 退出
     handleLogout() {
       // 删除sessionStorage中的token
