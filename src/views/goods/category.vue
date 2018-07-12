77995cdcb4701e1e377e335515c0650e1e69deb2
ChunYnag <yangchun920726@126.com>
2018-07-12 22:58:11 +0800

    商品分类页面展示

--- src/views/goods/category.vue
+++ src/views/goods/category.vue
@@ -11,22 +11,31 @@
 
     <!-- 表格 -->
     <el-table
+      height="700px"
       border
-      :data="tableData"
+      :data="list"
       style="width: 100%">
       <el-table-column
-        prop="date"
+        prop="cat_name"
         label="分类名称"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="name"
         label="级别"
         width="180">
+        <template slot-scope="scope">
+          <span v-if="scope.row.cat_level === 0">一级</span>
+          <span v-if="scope.row.cat_level === 1">二级</span>
+          <span v-if="scope.row.cat_level === 2">三级</span>
+        </template>
       </el-table-column>
       <el-table-column
-        prop="address"
         label="是否有效">
+        <template slot-scope="scope">
+           <!-- <span v-if="scope.row.cat_deleted">有效</span>
+           <span v-else>无效</span> -->
+           {{ scope.row.cat_deleted ? '无效' : '有效' }}
+        </template>
       </el-table-column>
       <el-table-column
         prop="address"
@@ -43,7 +52,23 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: []
+    };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      const { data: resData } = await this.$http.get('categories?type=3');
+      // console.log(resData);
+      const { data } = resData;
+      // console.log(data);
+      this.list = data;
+    }
+  }
 };
 </script>
 
