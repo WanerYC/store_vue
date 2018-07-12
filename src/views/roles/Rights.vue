35815b4e1c8db5a73ea54cb1fb895f53c1315fb7
ChunYnag <yangchun920726@126.com>
2018-07-12 14:24:10 +0800

    面包屑提取

--- src/views/roles/Rights.vue
+++ src/views/roles/Rights.vue
@@ -1,11 +1,7 @@
 <template>
   <el-card class="box-card">
     <!-- 面包屑 -->
-    <el-breadcrumb separator-class="el-icon-arrow-right" class="elbreadcrumb">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
-      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
-    </el-breadcrumb>
+    <my-breadcrumb level1="权限管理" level2="权限列表"></my-breadcrumb>
 
     <!--  内容表格 -->
     <el-table
