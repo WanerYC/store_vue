0e637d61a36a99d2eec40f3b6a9af5890893fbcb
ChunYnag <yangchun920726@126.com>
2018-07-15 20:30:57 +0800

    富文本编辑页
    npm install vue-quill-editor

--- src/views/goods/addGoodsList.vue
+++ src/views/goods/addGoodsList.vue
@@ -30,19 +30,50 @@
              <!-- 三级联动组件 -->
              <categoryCascader @gaibianle="handlegaibianle" type="3"></categoryCascader>
           </el-form-item>
-          <el-form-item>
+          <!-- <el-form-item>
             <el-button type="primary" @click="handleAdd">立即创建</el-button>
             <el-button>取消</el-button>
-          </el-form-item>
+          </el-form-item> -->
         </el-form>
+        <el-button>下一步</el-button>
       </el-tab-pane>
-      <el-tab-pane label="商品图片">商品图片</el-tab-pane>
-      <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+      <el-tab-pane label="商品图片">
+        商品图片
+      </el-tab-pane>
+
+      <!-- 富文本编译区 -->
+      <el-tab-pane label="商品详情">
+        <quill-editor
+          class="quillEditor"
+          v-model="form.goods_introduce"
+          ref="myQuillEditor"
+          :options="editorOption"
+          @blur="onEditorBlur($event)"
+          @focus="onEditorFocus($event)"
+          @ready="onEditorReady($event)">
+        </quill-editor>
+        <el-row>
+          <el-col :span="8" class="Btns">
+            <el-button type="primary" @click="handleAdd">立即创建</el-button>
+            <el-button type="info">取消</el-button>
+          </el-col>
+        </el-row>
+        <!-- <el-button type="primary" @click="handleAdd">立即创建</el-button>
+        <el-button type="info">取消</el-button> -->
+      </el-tab-pane>
     </el-tabs>
    </el-card>
 </template>
 
 <script>
+// 富文本编辑器
+// require styles
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
+import 'quill/dist/quill.bubble.css';
+
+import { quillEditor } from 'vue-quill-editor';
+
 // 引入三级联动
 import categoryCascader from '@/components/CategoryCascader.vue';
 export default {
@@ -53,12 +84,17 @@ export default {
         goods_price: '',
         goods_weight: '',
         goods_number: '',
-        goods_cat: ''
+        goods_cat: '',
+        goods_introduce: ''
       }
+      // editorOption: {
+      //   // some quill options
+      // }
     };
   },
   components: {
-    categoryCascader
+    categoryCascader,
+    quillEditor
   },
   methods: {
     async handleAdd() {
@@ -80,7 +116,33 @@ export default {
       // console.log(data);
       this.form.goods_cat = data.join(',');
       // console.log(this.form.goods_cat);
+    },
+
+    // 富文本编辑器方法
+    // 文本框失去焦点时触发
+    onEditorBlur(quill) {
+      console.log('editor blur!', quill);
+      console.log(this.form.goods_introduce);
+    },
+    // 文本框获取焦点时触发
+    onEditorFocus(quill) {
+      console.log('editor focus!', quill);
+    },
+    onEditorReady(quill) {
+      console.log('editor ready!', quill);
+    },
+    onEditorChange({ quill, html, text }) {
+      console.log('editor change!', quill, html, text);
+      this.content = html;
     }
+  // },
+  // computed: {
+  //   editor() {
+  //     return this.$refs.myQuillEditor.quill;
+  //   }
+  // },
+  // mounted() {
+  //   console.log('this is current quill instance object', this.editor);
   }
 };
 </script>
@@ -94,4 +156,12 @@ export default {
   height: 100%;
   overflow: auto
 }
+
+/* .quillEditor {
+  height: 300px;
+} */
+
+/* .Btns {
+  margin: 10px 0;
+} */
 </style>
