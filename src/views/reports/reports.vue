cf7544716be1182cc4e59e48b77fbd51c3e60865
ChunYnag <yangchun920726@126.com>
2018-07-17 23:39:38 +0800

    echarts

--- src/views/reports/reports.vue
+++ src/views/reports/reports.vue
@@ -1,37 +1,46 @@
 <template>
-  <el-card class="box-card">
+  <el-card class='box-card'>
     <!-- 面包屑 -->
-    <my-breadcrumb level1="数据统计" level2="数据报表"></my-breadcrumb>
+    <my-breadcrumb level1='数据统计' level2='数据报表'></my-breadcrumb>
+    <div>
+      <p>reports componet</p>
+      <div id='main' style='width: 600px; height:400px'></div>
+    </div>
   </el-card>
 </template>
 
 <script>
+import echarts from 'echarts';
 // import 'echarts.min.js';
 // var myChart = echarts.init(document.getElementById('main'));
 export default {
   data() {
-  //   methods: {
-  //     $.get('data.json').done(function (data) {
-  //       myChart.setOption({
-  //           title: {
-  //               text: '异步数据加载示例'
-  //           },
-  //           tooltip: {},
-  //           legend: {
-  //               data:['销量']
-  //           },
-  //           xAxis: {
-  //               data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-  //           },
-  //           yAxis: {},
-  //           series: [{
-  //               name: '销量',
-  //               type: 'bar',
-  //               data: [5, 20, 36, 10, 10, 20]
-  //           }]
-  //       });
-  //     });
-  //   }
+    return {};
+  },
+  mounted() {
+    var myChart = echarts.init(document.getElementById('main'));
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
   }
 };
 </script>
